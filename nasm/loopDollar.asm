a0ac6fcd15a57a7690a3457b15321eb53927c56e
Bruno Bernardo <bigsby@live.com>
2018-05-29 17:23:24 +0100

    Add comments

--- nasm/loopDollar.asm
+++ nasm/loopDollar.asm
@@ -1,3 +1,5 @@
-jmp $
-times 510-($-$$) db 0
+nop
+jmp $-1 ; jump to current position (enternal loop)
+times 510-($-$$) db 0  ; $  = current position
+                       ; $$ = begginng of the current section
 dw 0xaa55
